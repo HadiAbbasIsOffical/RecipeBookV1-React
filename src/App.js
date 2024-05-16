fde13fc129e1c09020ebc324b7f086c2d05219e5
Alien gamer <HadiAbbasIsOffical@users.noreply.github.com>
2024-05-16 11:14:27 +0500

    added channel homepage

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route
-            path='/'
+            path='/RecipeBookV1-React'
             element={<About />} 
           />
           <Route
